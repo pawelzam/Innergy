27f67c49454e625ef6a2382a95c01da1fbb1500a
Pawel Zamrij <PZAM@danskebank.dk>
2024-11-05 11:18:10 +0100

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 export type ServiceYear = 2020 | 2021 | 2022;
 export type ServiceType = "Photography" | "VideoRecording" | "BlurayPackage" | "TwoDayEvent" | "WeddingSession";
 
+import serviceData from "../assets/services.json";
+
 export const updateSelectedServices = (
     previouslySelectedServices: ServiceType[],
     action: { type: "Select" | "Deselect"; service: ServiceType }
-) => [];
+): ServiceType[] => {
+    const { type, service } = action;
+    if (type === "Select" && !previouslySelectedServices.includes(service)) {
+        return [...previouslySelectedServices, service];
+    } else if (type === "Deselect") {
+        return previouslySelectedServices.filter(s => s !== service);
+    }
+    return previouslySelectedServices;
+};
 
-export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
+export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => {
+    let basePrice = 0;
+    let finalPrice = 0;
+
+    selectedServices.forEach(service => {
+        basePrice += serviceData.services[selectedYear.toString()].individualServices[service];
+    });
+
+    const yearBundles = serviceData.services[selectedYear.toString()].bundles;
+    yearBundles.forEach(bundle => {
+        const isBundleSelected = bundle.services.every(bService => selectedServices.includes(bService as ServiceType));
+        if (isBundleSelected && (finalPrice === 0 || bundle.cost < finalPrice)) {
+            finalPrice = bundle.cost;
+        }
+    });
+
+    if (finalPrice === 0) {
+        finalPrice = basePrice;
+    }
+
+    return { basePrice, finalPrice };
+};
